6dda478948e6c8198c8b9309a69d3c65e81b6017
rizkysetiawan02736 <63201220+rizkysetiawan02736@users.noreply.github.com>
2024-04-15 03:49:00 +0800

    tambah nama

--- admin-ui/src/App.jsx
+++ admin-ui/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <div className="h3">Muhammad Rizky Setiawan</div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
